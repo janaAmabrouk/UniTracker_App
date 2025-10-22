3c3e666fa810819249c425f0569848f3b1a95f89
janaAmabrouk <110740808+janaAmabrouk@users.noreply.github.com>
2025-10-22 23:41:24 +0300

    Delete UniTracker_front-end_only directory

--- UniTracker_front-end_only/admin_web/lib/config/supabase_config.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseConfig {
-  static const String supabaseUrl = 'https://fsdopmaaeqkxmirbvheu.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZG9wbWFhZXFreG1pcmJ2aGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNTE4NTMsImV4cCI6MjA2NDcyNzg1M30.2rAeKQFEDUsLB9X3XqtoBp9b7Twen_cVBU0JLvwI65s';
-}
--- UniTracker_front-end_only/unitracker/android/app/src/main/kotlin/com/example/unitracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.unitracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
